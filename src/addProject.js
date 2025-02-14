64c1e0dea83884d3c31e507f75307ed48d3b224d
Jordan Leonard <leonard_2010@live.com>
2025-02-13 21:23:19 -0600

    added tag for tasks

--- src/addProject.js
+++ src/addProject.js
@@ -1,19 +1,24 @@
 import { addProjectForm } from "./addProjectForm";
 import { domDesign } from "./dom";
 
-export function addProject (title, desc, dueDate, priority) {
+export function addProject (title, desc, dueDate, priority, tag) {
     class Project {
         constructor(title, desc, dueDate, priority) {
             this.title = title;
             this.desc = desc;
             this.dueDate = dueDate;
             this.priority = priority;
+            this.tag = tag;
         }
         projectCreate() {
             const projectDiv = document.getElementById('project-list');
+            const taskDiv = document.getElementById('task-list');
 
             const projectContainer = document.createElement('div');
             projectContainer.style.border = '1px solid black';
+
+            const tagLine = document.createElement('p');
+            tagLine.textContent = `${this.tag}`;
 
             const projecth1 = document.createElement('h1');
             projecth1.textContent = `${this.title}`;
@@ -33,13 +38,26 @@ export function addProject (title, desc, dueDate, priority) {
             const projectDelete = document.createElement('button');
             projectDelete.textContent = 'Delete';
 
-            projectDiv.appendChild(projectContainer);
-            projectContainer.appendChild(projecth1);
-            projectContainer.appendChild(projectDesc);
-            projectContainer.appendChild(projectDueDate);
-            projectContainer.appendChild(projectPriority);
-            projectContainer.appendChild(projectEdit);
-            projectContainer.appendChild(projectDelete);
+            if(tag !== ''){
+                taskDiv.appendChild(projectContainer);
+                projectContainer.appendChild(tagLine);
+                projectContainer.appendChild(projecth1);
+                projectContainer.appendChild(projectDesc);
+                projectContainer.appendChild(projectDueDate);
+                projectContainer.appendChild(projectPriority);
+                projectContainer.appendChild(projectEdit);
+                projectContainer.appendChild(projectDelete);
+            }else {
+                projectDiv.appendChild(projectContainer);
+                projectContainer.appendChild(projecth1);
+                projectContainer.appendChild(projectDesc);
+                projectContainer.appendChild(projectDueDate);
+                projectContainer.appendChild(projectPriority);
+                projectContainer.appendChild(projectEdit);
+                projectContainer.appendChild(projectDelete);
+            }
+
+         
 
             let projects = JSON.parse(localStorage.getItem('projectData')) || [];
 

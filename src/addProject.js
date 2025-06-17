58e64b5ab640d57febfa50e801117aa1797bf7ed
Jordan Leonard <leonard_2010@live.com>
2025-06-16 21:33:25 -0500

    removed anything with tag

--- src/addProject.js
+++ src/addProject.js
@@ -1,24 +1,19 @@
 import { addProjectForm } from "./addProjectForm";
 import { domDesign } from "./dom";
 
-export function addProject (title, desc, dueDate, priority, tag) {
+export function addProject (title, desc, dueDate, priority) {
     class Project {
         constructor(title, desc, dueDate, priority) {
             this.title = title;
             this.desc = desc;
             this.dueDate = dueDate;
             this.priority = priority;
-            this.tag = tag;
         }
         projectCreate() {
             const projectDiv = document.getElementById('project-list');
-            const taskDiv = document.getElementById('task-list');
 
             const projectContainer = document.createElement('div');
             projectContainer.style.border = '1px solid black';
-
-            const tagLine = document.createElement('p');
-            tagLine.textContent = `#${this.tag}`;
 
             const projecth1 = document.createElement('h1');
             projecth1.textContent = `${this.title}`;
@@ -43,28 +38,15 @@ export function addProject (title, desc, dueDate, priority, tag) {
             const projectDelete = document.createElement('button');
             projectDelete.textContent = 'Delete';
 
-            if(tag){
-                taskDiv.appendChild(projectContainer);
-                projectContainer.appendChild(tagLine);
-                projectContainer.appendChild(projecth1);
-                projectContainer.appendChild(projectDesc);
-                projectContainer.appendChild(projectDueDate);
-                projectContainer.appendChild(projectPriority);
-                projectContainer.appendChild(completedLabel)
-                projectContainer.appendChild(completedCheck);
-                projectContainer.appendChild(projectEdit);
-                projectContainer.appendChild(projectDelete);
-            }else {
-                projectDiv.appendChild(projectContainer);
-                projectContainer.appendChild(projecth1);
-                projectContainer.appendChild(projectDesc);
-                projectContainer.appendChild(projectDueDate);
-                projectContainer.appendChild(projectPriority);
-                projectContainer.appendChild(completedLabel);
-                projectContainer.appendChild(completedCheck);
-                projectContainer.appendChild(projectEdit);
-                projectContainer.appendChild(projectDelete);
-            }
+            projectDiv.appendChild(projectContainer);
+            projectContainer.appendChild(projecth1);
+            projectContainer.appendChild(projectDesc);
+            projectContainer.appendChild(projectDueDate);
+            projectContainer.appendChild(projectPriority);
+            projectContainer.appendChild(completedLabel);
+            projectContainer.appendChild(completedCheck);
+            projectContainer.appendChild(projectEdit);
+            projectContainer.appendChild(projectDelete);
 
             //need to have project delete go into local storage and delete task as well
             //event listeners
@@ -91,6 +73,6 @@ export function addProject (title, desc, dueDate, priority, tag) {
             localStorage.setItem('projectData', JSON.stringify(projects));
         }
     }
-    const createProject = new Project(title, desc, dueDate, priority, tag);
+    const createProject = new Project(title, desc, dueDate, priority);
     createProject.projectCreate();
 }
